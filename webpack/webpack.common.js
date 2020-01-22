99108658d9ed215964d9d2eaba53164a90b7ebec
Runqi Luo <richie.runqi@gmail.com>
2020-01-21 22:18:22 -0500

    revert back

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -60,7 +60,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `'http://18.191.220.96:8080'`
+                SERVER_API_URL: `'https://18.191.220.96:8080'`
             }
         }),
         new CopyWebpackPlugin([
